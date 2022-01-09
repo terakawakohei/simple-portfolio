4f15a66aced2b80db34d68b15c555909ebc8d0ba
terakawakohei <trivover@icloud.com>
2022-01-09 13:57:18 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./portfolio-icon.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>terakawa kohei</h1>
+        <p>this is my portfolio</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
